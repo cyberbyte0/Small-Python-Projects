5aadea4c02516e2dbfd461ebd84af82e045b7758
M Fareed Akram <akramfareed.m@gmail.com>
2024-03-04 12:23:47 +0530

    Create Number Guessing game.py
    A number Guessing game using simple 'if else and while loop ' functions

---
+++ Number Guessing game.py
@@ -0,0 +1,31 @@
+import random
+print('Wellcome to my Number Guessing game :) ')
+start = input('Type yes/y to start and quit/q to Quit the game.\n ').lower()
+if start not in ['yes','y']:
+    print('Thank you for playing')
+    quit()
+
+print('let\'s begain!!!')
+low = input('Enter the lower limit: ')
+if low.isdigit()== False:
+    print('Please enter a valid number')
+    quit()
+high = input('Enter the higher limit: ')
+if high.isdigit() == False:
+    print('Please enter a valid number')
+    if high <= low:
+        print('The higher limit should be greater than the lower limit')
+    quit()
+number = random.randint(int(low), int(high)+1)
+print(f'Guess the number between {low} and {high}')
+while True:
+    guess = int(input('Enter your guess: '))
+    if guess == number:
+        print('Congratulations! You have guessed the number correctly.')
+        break
+    elif guess < number:
+        print('Your guess is too low')
+    else:
+        print('Your guess is too high')
+
+print('Thank you for playing my number guessing game :)')
