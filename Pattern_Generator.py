fe6d93d073bb6923bd11db035bded40c5e50c93a
M Fareed Akram <akramfareed.m@gmail.com>
2024-10-21 19:05:55 +0530

    Pattern_Generator.py
    Added option to print all shapes
    And wrote functions for each input such as shape, height and char...

--- Pattern_Generator.py
+++ Pattern_Generator.py
@@ -1,6 +1,6 @@
 class Pattrens:
     
-    designs = [ "1) Square","2) Triangle"]
+    designs = [ "1) Square","2) Triangle","3) All Shapes"]
     triangle =['1) Right Half Triangel','2) Left Half Triangel','3) Equelateral Triangel']
     
     def __init__(self) -> None:
@@ -12,60 +12,78 @@ class Pattrens:
     def get_inputs(self):
         for i in (self.designs):
             print(i)
+        self.shapes(self.get_shape(),self.get_height(),self.get_char())
+
+
+    def get_shape(self):
         self.shape = input("select shape/number from the above: ").lower()
-        self.shapes(self.shape)
+        if self.shape not in '123':
+            return self.get_shape()
+        return self.shape
+
+    def get_height(self):
+        self.height = int(input('Enter height: '))
+        if self.height <= 0:
+            return self.get_height()
+        return self.height
+
+    def get_char(self):
+        self.char = input("Enter your desierd char or num: ")[0]
+        return self.char
 
 
-    # def all_shapes(self, length, char):
-    #     self.square( length,char)
-
-    #     for type in range (1,4):
-    #         self.triangels(type,length,char)
-
+    def all_shapes(self, height, char):
+        self.square( height,char)
+        for model in range (1,4):
+            self.triangels(str(model),height,char)
 
 
-    def shapes(self,shape):
-        length = int(input('Enter Lenght: '))
-        char = input("Enter your desierd char or num: ")
+    def shapes(self,shape,height,char):
+        print(type(shape), shape)
+        print(type(height),height)
+        print(type(char),char)
         if shape == "square" or shape == '1':
-            self.square(length, char)
+            self.square(height, char)
 
         elif shape == "triangle" or shape == '2':
             for i in self.triangle:
                 print(i)
-            type = input("Select the  Number for the type of Triangle: ").lower()
-            self.triangels(type ,length,char)
+            model = input("Select the  Number for the model of Triangle: ").lower()
+            self.triangels(model ,height,char)
+
+        elif shape == "ALL shapes" or shape == '3':
+            self.all_shapes(height, char)
 
         else:
-            print("invalid selections")
+            print("invalid selections for shape")
             self.controls()
 
-    def square(self, length,char,):
-        for input in range(length):
-            print(char * length)
-        self.controls()
-            
 
-    def triangels(self,type,length,char):
-        if type == 'Right1 Half Triangel' or type == '1':
+    def square(self, height,char,):
+        for input in range(height):
+            print(char * height)
+        
+            
+    def triangels(self,model,height,char):
+        if model == 'Right Half Triangel' or model == '1':
             print("Right Half Triangel")
-            for input in range(1,length+1):
+            for input in range(1,height+1):
                 print(char * input)
 
-        elif type == 'Left Half Triangel' or type == "2":
+        elif model == 'Left Half Triangel' or model == "2":
             print("Left Half Triangel")
-            for input in range (1, length+1):
-                print((" " * (length-input)) + (char * input))
+            for input in range (1, height+1):
+                print((" " * (height-input)) + (char * input))
                 
-        elif type == 'Equalateral Triangle' or type == '3':
+        elif model == 'Equalateral Triangle' or model == '3':
             print("Equelateral Triangel")
-            for input in range(1,(length*2)):
+            for input in range(1,(height*2)):
                 if input % 2 != 0:
-                    print( ( " " * (length - (input//2))) + '@' * input)
+                    print( ( " " * (height - (input//2))) + char * input)
 
         else:
             print("invalid selection")
-        self.controls()
+            self.controls()
 
 
     def controls(self):
@@ -73,6 +91,7 @@ class Pattrens:
 
         if start == '1' or start =="start":
             self.get_inputs()
+            return self.controls()
 
         else:
             print("Thank you ")
